8b604c3f9777adc79ab74ab0a8718cb71425f5a1
giovannimedici <68286314+giovannimedici@users.noreply.github.com>
2020-07-15 11:03:41 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace projeto_teste
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadLine();
+        }
+    }
+}
